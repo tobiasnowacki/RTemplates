418069cf14292d4e12014d5d9eda22db71c95004
tobiasnowacki <tobynowacki@gmail.com>
2020-02-07 13:39:15 -0800

    plottheme

---
+++ plottheme.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(extrafont)
+library(viridis)
+
+font_import()
+
+# Define ggplot theme
+theme_tn <- function(){
+  theme_bw(base_size=11) %+replace%
+  theme(
+    text = element_text(family = "Roboto"),
+    panel.grid.major =  element_line(
+      colour = "grey50",
+      size = 0.2,
+      linetype = "dotted"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "grey97"),
+    plot.margin = unit(c(0.2, 1, 0.2, 1), "cm"),
+    legend.margin = margin(0, 0, 0, 0),
+    legend.title = element_text(size = 10, face = "bold"),
+    strip.background = element_rect(fill= NULL, colour = "white", linetype = NULL),
+    strip.text = element_text(colour = 'grey50', size = 9, vjust = 0.5),
+    axis.text = element_text(family = "Roboto Condensed")
+  )
+}
+
+# Colourblind palette for plots etc.
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
